35bce96fd06c410687e4bffb828dfa860371c912
leonardo <leolimaasantos@gmail.com>
2026-06-10 22:27:56 -0300

    adds syntax to .vimrc and enables user input and .vimrc backup on script.sh

--- script.sh
+++ script.sh
@@ -1,11 +1,23 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+check_dependencies() {
+  echo "Checking dependencies..."
+  local deps=(curl git)
+  for cmd in "${deps[@]}"; do
+    if ! command -v "$cmd" &>/dev/null; then
+      echo "Error: '$cmd" is required but not installed. Exiting.
+      exist 1
+    fi
+  done
+  echo "All dependencies avaliable! Running the script..."
+}
+
 ask_to_execute() {
   local msg="$1"
   local ans_raw=""
 
-  read -p "$msg [Y/n]:" ans_raw
+  read -p "$msg [Y/n]:" ans_raw </dev/tty
 
   if [ -z "$ans_raw" ]; then
     ans_raw="y"
@@ -26,11 +38,22 @@ ask_to_execute() {
 
 setup_vim() {
   echo "    Setting up Vim Config"
+
   curl -fsSl "https://raw.githubusercontent.com/leolimasnts/lab-setup/main/.vimrc" -o "$HOME/.vimrc"
-  echo "    Vim Config setted up successfully!"
+
+  if [ -f "$HOME/.vimrc" ]; then
+    mv "$HOME/.vimrc" "$HOME/.vimrc.bak"
+    echo "    Backed up existing .vimrc to .vimrc.bak"
+  fi
+
+  echo "    Vim Config set up successfully!"
 }
 
 setup_font() {
+  if ! command -v fc-cache $ >/dev/null; then
+    echo "    Skipping fonts: fc-cache tool not found"
+    return 0
+  fi
   echo "    Downloading fonts..."
 
   local tmp_dir
@@ -40,21 +63,23 @@ setup_font() {
 
   mkdir -p "$HOME/.local/share/fonts"
 
-  git clone "https://github.com/leolimasnts/lab-setup" "$tmp_dir"
+  git clone --depth 1 "https://github.com/leolimasnts/lab-setup" "$tmp_dir"
   cp "$tmp_dir/fonts/"* "$HOME/.local/share/fonts"
 
   fc-cache -f -v
-
-  rm -rf "$tmp_dir"
-  trap - EXIT
   echo "    Fonts installed successfully!"
 }
 
 setup_keyboard() {
+  if [ -z "${DISPLAY:-}" ]; then
+    echo "Skipping keyboard remap: No X11 Display detected"
+    return 0
+  fi
   echo "    Remapping CapsLock to Esc..."
   xmodmap -e "clear Lock"
   setxkbmap -option caps:escape
   echo "    Keyboard remapped successfully!"
+  echo "    To revert, run: setxkbmap -option"
 }
 
 main() {
